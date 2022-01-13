75f1fa5e05c27a373d378e8be5775daf4d7a5476
307372 <307372@uwr.edu.pl>
2022-01-13 19:23:35 +0100

    - Settings are now saved between sessions. - "Hide word" functionality is now working.

--- app/src/main/java/com/example/granap/GameActivity.java
+++ app/src/main/java/com/example/granap/GameActivity.java
@@ -1,21 +1,44 @@
 package com.example.granap;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.granap.SettingsActivity.HIDE_WORD;
+import static com.example.granap.SettingsActivity.SHARED_PREFERENCES;
+import static com.example.granap.SettingsActivity.WORD_LENGTH_MAX;
+import static com.example.granap.SettingsActivity.WORD_LENGTH_MIN;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.FileInputStream;
 
 public class GameActivity extends AppCompatActivity {
 
-    FileInputStream fileInputStream;
     TextView tvRandomWord;
+    ConstraintLayout backgroundDiv;
+
     TypedArray dictionary;
+
+    int wordLengthMax;
+    int wordLengthMin;
+    boolean hideWord;
+
+    CountDownTimer timer = new CountDownTimer(1000, 100) {
+        @Override public void onTick(long x) {}
+        public void onFinish() {setWordInvisible();}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,62 @@ public class GameActivity extends AppCompatActivity {
 
         dictionary = getResources().obtainTypedArray(R.array.dict);
         tvRandomWord = findViewById(R.id.tvRandomWord);
+        backgroundDiv = findViewById(R.id.backgroundDiv);
+
+        backgroundDiv.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (hideWord) {
+                    timer.cancel();
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            // Pressed down
+                            setWordVisible();
+                            return true;
+                        case MotionEvent.ACTION_UP:
+                            // Released
+                            setWordInvisible();
+                            return true;
+                        case MotionEvent.ACTION_CANCEL:
+                            // Released - Dragged finger outside
+                            setWordInvisible();
+                            return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setWordVisible()
+    {
+        tvRandomWord.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+    }
+
+    public void setWordInvisible()
+    {
+        tvRandomWord.setInputType(InputType.TYPE_CLASS_TEXT |InputType.TYPE_TEXT_VARIATION_PASSWORD);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadSettings();
+        if (hideWord)
+        {
+            setWordInvisible();
+        }
+        else {
+            tvRandomWord.setInputType(InputType.TYPE_CLASS_TEXT);
+        }
+    }
+
+    public void showWordFor1s()
+    {
+        timer.cancel();
+        setWordVisible();
+        timer.start();
     }
 
     public void rerollWord(View x)
@@ -31,7 +110,17 @@ public class GameActivity extends AppCompatActivity {
         int max = dictionary.length();
         int index = (int) Math.floor(Math.random()*max);
         tvRandomWord.setText(dictionary.getString(index));
-        int a =0;
+        if (hideWord) {
+            showWordFor1s();
+        }
+    }
+
+    public void loadSettings()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        wordLengthMax = Integer.parseInt(sharedPreferences.getString(WORD_LENGTH_MAX, "20"));
+        wordLengthMin = Integer.parseInt(sharedPreferences.getString(WORD_LENGTH_MIN, "1"));
+        hideWord = sharedPreferences.getBoolean(HIDE_WORD, false);
     }
 
     public void showSettings(View x)
--- app/src/main/java/com/example/granap/SettingsActivity.java
+++ app/src/main/java/com/example/granap/SettingsActivity.java
@@ -2,14 +2,59 @@ package com.example.granap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFERENCES = "sharedPrefs";
+    public static final String WORD_LENGTH_MAX = "WordLengthMax";
+    public static final String WORD_LENGTH_MIN = "WordLengthMin";
+    public static final String HIDE_WORD = "HideWord";
+
+    EditText iWordLengthMax;
+    EditText iWordLengthMin;
+    Switch swHideWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        iWordLengthMax = findViewById(R.id.iWordLengthMax);
+        iWordLengthMin = findViewById(R.id.iWordLengthMin);
+        swHideWord = findViewById(R.id.swHideWord);
+
+        loadSettings();
     }
-    // Toast = popup taki co mówi coś i znika
+
+    public void loadSettings()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        iWordLengthMax.setText(sharedPreferences.getString(WORD_LENGTH_MAX, "20"));
+        iWordLengthMin.setText(sharedPreferences.getString(WORD_LENGTH_MIN, "1"));
+        swHideWord.setChecked(sharedPreferences.getBoolean(HIDE_WORD, false));
+    }
+
+    public void saveSettings()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(WORD_LENGTH_MAX, iWordLengthMax.getText().toString());
+        editor.putString(WORD_LENGTH_MIN, iWordLengthMin.getText().toString());
+        editor.putBoolean(HIDE_WORD, swHideWord.isChecked());
+
+        editor.apply();
+    }
+
+    public void saveSettingsButton(View x)
+    {
+        saveSettings();
+        Toast.makeText(this, "Ustawienia zapisane", Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
